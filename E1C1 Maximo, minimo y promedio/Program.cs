2948db359e9728bad0ba68cbc2c3c769d2808bf3
TobiasMoretti123 <78236685+TobiasMoretti123@users.noreply.github.com>
2022-08-24 21:29:28 -0300

    Resuelto

---
+++ E1C1 Maximo, minimo y promedio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace E1C1_Maximo__minimo_y_promedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int maximo = int.MinValue;
+            int minimo = int.MaxValue;
+            float promedio;
+            int suma = 0;
+            int i = 0;
+
+            for ( ; i<5; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numeroIngresado = int.Parse(Console.ReadLine());
+
+                if(numeroIngresado < minimo)
+                {
+                    minimo = numeroIngresado;
+                }
+
+                if(numeroIngresado > maximo)
+                {
+                    maximo = numeroIngresado;
+                }
+
+                suma += numeroIngresado;
+            }
+
+            promedio = (float)suma / i;
+
+            Console.WriteLine($"Maximo: {maximo}\nMinimo: {minimo}\nPromedio: {promedio}");
+        }
+    }
+}
---
+++ E2C1 Error al cubo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace E2C1_Error_al_cubo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado = 0;
+            int numeroCuadrado;
+            int numeroCubo;
+
+            Console.WriteLine("Ingresar un numero: ");
+
+            if(int.TryParse(Console.ReadLine(),out numeroIngresado) && numeroIngresado > 0)
+            {
+                numeroCuadrado = (int)Math.Pow(numeroIngresado, 2);
+                numeroCubo = (int)Math.Pow(numeroIngresado, 3);
+
+                Console.WriteLine($"Numero: {numeroIngresado}\nNumero al cuadrado: {numeroCuadrado}\nNumero al cubo: {numeroCubo}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+            }
+        }
+    }
+}
